e0c95cf0dcfe371480d3104f55bd0c41a767231f
Hamid-0 <hamidabol200078@gmail.com>
2022-03-12 13:18:44 -0500

    Updated styling, added remaining links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,8 +5,10 @@ export default function Navbar() {
     return (
         <nav>
         <Link to="/">Home</Link>
-        <Link to="/page-2">page 2</Link>
+        <Link to="/page-2">Page 2</Link>
         <Link to="/using-typescript">Using Type Script</Link>
+        <Link to="/using-ssr">Using SSR</Link>
+        <Link to="/using-dsg">Using DSG</Link>
 
         </nav>
             
